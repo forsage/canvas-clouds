b225bca1d0b6dac994e0882ba81e193a09a6daa6
dajkap@gmail.com <dajkap@gmail.com@5c155198-a455-87fc-db41-4488f41b498e>
2012-07-21 20:27:34 +0000

    Adding easeljs test:
    I tried to port the current Kinetic version.

---
+++ easeljs_test/js/bubble.js
@@ -0,0 +1,53 @@
+/*
+*
+*	Example: prototyping - namespaces
+*
+*/
+
+
+$(document).ready( function (){
+
+	// alert('fut');
+	var testBubble	= new game.Bubble( 30, 30, Common.getRandomColor() );
+
+});
+
+// initialize the namespace
+// var game = game || {};
+
+
+// this "outer/empty" function is needed becuase the inner functions, variables shouldn't generate into global namespace
+(function () {
+
+	var Bubble	= function ( inX, inY, inColor ){
+		this.initialize( inX, inY, inColor );
+	}
+
+	var p	= Bubble.prototype;
+
+	p.x		= null;
+	p.y		= null;
+	p.color	= null;
+
+	p.initialize	= function( x, y, color ){
+		this.x	= x;
+		this.y	= y;
+		this.color	= color;
+
+		// alert(this.color);
+	}
+
+
+	p.create	= function (){
+
+	}
+
+
+
+// connect the Bubble to the game namespace
+game.Bubble	= Bubble;
+
+}
+// This line will generate the game namespace if it doesn't exists.
+(this.game = this.game		 || {}));
+
